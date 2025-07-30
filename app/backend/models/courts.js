d98d290090919a0eb3dfeb0b0a3b151af64d5a55
Unredix <999daved@gmail.com>
2025-07-31 00:01:47 +0200

    Add new SVG icons for air conditioner, coffee beans, lockers, parking circle, restroom, router with WiFi, shop, shower, and toilet

--- app/backend/models/courts.js
+++ app/backend/models/courts.js
@@ -7,6 +7,47 @@ const courtSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    type: {
+      type: String,
+      required: true,
+      enum: [
+        "Tennis",
+        "Basketball",
+        "Badminton",
+        "Squash",
+        "Volleyball",
+        "Padel",
+      ],
+    },
+    accessibility: {
+      type: String,
+      required: true,
+      enum: [
+        "Wheelchair Accessible",
+        "Not Wheelchair Accessible",
+        "Limited Accessibility",
+        "No Accessibility Features",
+      ],
+    },
+    equipment: {
+      type: String,
+      required: true,
+      enum: ["Available for Rent", "Free", "Not Available"],
+    },
+    features: {
+      type: [String], // Array of features like "WiFi", "Lighting", etc
+      required: false,
+    },
+    lighting: {
+      type: String,
+      required: true,
+      enum: ["Daylight", "Artificial", "Both", "None"],
+    },
+    indoorOutdoor: {
+      type: String,
+      required: true,
+      enum: ["Indoor", "Outdoor", "Both"],
+    },
     location: {
       type: String,
       required: true,
@@ -14,10 +55,12 @@ const courtSchema = new mongoose.Schema(
     surface: {
       type: String,
       required: true,
+      enum: ["Clay", "Grass", "Hard", "Carpet", "Wood"],
     },
     numofCourts: {
       type: Number,
       required: true,
+      min: 1, // At least one court
     },
     availableTimes: {
       type: [String], // Array of time slots in HH:MM format
